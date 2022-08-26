88935fffbd311d05cd9eb671e5ce826cde9db52c
Nereida Rodríguez <nereida@LAPTOP-VAJBTHLP.localdomain>
2022-08-26 20:39:13 +0200

    added basic structure

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+//Estilos (solo 1)
+import "../styles/App.scss";
+//Librerías, datos y LS
+import { Link, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import ls from "../services/localStorage";
+//Componentes
+
+function App() {
+  //VARIABLES DE ESTADO
+  //FUNCIONES AUXILIARES
+  //EVENTOS
+  //RETURN
+  return (
+    <div className="App">
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
