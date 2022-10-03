c7f05599b701299c6f968b557994915495833e9b
Nereida Rodríguez <nereida@LAPTOP-VAJBTHLP.localdomain>
2022-10-03 12:16:28 +0200

    tried to fix the styles

--- src/components/FilterBySpecies.js
+++ src/components/FilterBySpecies.js
@@ -1,19 +1,23 @@
+import "../styles/layout/FilterBySpecies.scss";
 function FilterBySpecies(props) {
   const handleChange = (ev) => {
     props.handleFilterBySpecies(ev.target.value);
   };
 
   return (
-    <>
-      <label htmlFor="species">Search by species:</label>
+    <div className="fbs__wrapper">
+      <label className="fbs__label" htmlFor="species">
+        Search by species:
+      </label>
       <input
+        className="fbs__input"
         type="text"
         name="species"
         id="species"
         value={props.speciesFilter}
         onChange={handleChange}
       ></input>
-    </>
+    </div>
   );
 }
 
